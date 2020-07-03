d30f7bd84c153f20ff0e186a38c9656a7fc60879
fmanier <contact@webtotem.fr>
2020-07-03 18:53:36 +0200

    fix(db) : migrations files
    - see above

---
+++ src/Migrations/Version20200701125259.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200701125259 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE contacts CHANGE is_active is_active TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE dates CHANGE entreprise_id entreprise_id INT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE contacts CHANGE is_active is_active TINYINT(1) DEFAULT \'1\' NOT NULL');
+        $this->addSql('ALTER TABLE dates CHANGE entreprise_id entreprise_id INT NOT NULL');
+    }
+}
